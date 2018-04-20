8c883585842dc12d897c7bcbb424dcda1790b6b9
雪竺轩 <ljm9104@126.com>
2018-04-20 14:59:34 +0800

    chapter01

--- chapter01/monkey_test.py
+++ chapter01/monkey_test.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
+
 class Student:
     def say(self):
         print("i am student")
 
+stu = Student()
 
 def say_teacher():
     print("i am teacher")
 
-
-stu = Student()
 stu.say = say_teacher
+
 stu.say()

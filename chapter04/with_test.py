845862b929817be536f26fb716ece4f9bff610fc
雪竺轩 <ljm9104@126.com>
2018-04-24 06:36:18 +0800

    chapter04

--- chapter04/with_test.py
+++ chapter04/with_test.py
@@ -1,30 +1,43 @@
-# 上下文管理器协议，实现了__enter__和__exit__魔法函数
-class Sample:
-    def __enter__(self):
-        print ("enter")
-        # 获取资源
-        return self
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        # 释放资源
-        print ("exit")
-    def do_something(self):
-        print ("doing something")
+"""
+with语句上下文管理协议实现__enter__和__exit__魔法函数
 
-with Sample() as sample:
-    sample.do_something()
+try except finally
+
+return语句执行，若finaly里有return执行，若没有就执行之前
+"""
+
+def exe_try():
+    try:
+        print ("code started")
+        raise KeyError
+        return 1
+    except KeyError as e:
+        print ("key error")
+        return 2
+    else:
+        print ("other error")
+        return 3
+    finally:
+        print ("finally")
+        # return 4           # return语句执行顺序，finaly没有return执行前面的，有就执行finaly的
 
 
-# 内置的上下文管理器,yield前面__enter__，后面__exit__
-import contextlib
+# 上下文管理器协议
+# class Sample:
+#     def __enter__(self):
+#         print ("enter")
+#         # 获取资源
+#         return self
+#     def __exit__(self, exc_type, exc_val, exc_tb):
+#         # 释放资源
+#         print ("exit")
+#     def do_something(self):
+#         print ("doing something")
 
+# with Sample() as sample:
+#     sample.do_something()
 
-@contextlib.contextmanager
-def file_open(file_name):
-    print ("file open")
-    yield {}                # 生成器
-    # yield 123                
-    print ("file end")
+if __name__ == "__main__":
+    result = exe_try()
+    print (result)
 
-with file_open("bobby.txt") as f_opened:
-    print ("file processing")
-print(f_opened)
